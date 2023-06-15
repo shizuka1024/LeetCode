25a0f28bbbf7c33018a55340489018e85f71e4f1
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2023-06-15 17:41:12 +0800

    Update 981.java

--- 981.java
+++ 981.java
@@ -1,3 +1,8 @@
+/*
+ * Leetcode Medium 981. Time Based Key-Value Store
+ * author: shizuka1024
+ * (Java)
+ */
 class TimeMap {
 
     Map<String , List<String>> valueM;
@@ -9,10 +14,26 @@ class TimeMap {
     }
     
     public void set(String key, String value, int timestamp) {
-        
+        if(!timeM.containsKey(key)){    //檢查key是否存在
+            valueM.put(key, new ArrayList<String>());
+            timeM.put(key, new ArryList<Integer>());
+        }
+        valueM.get(key).add(value);
+        timeM.get(key).add(timestamp);
     }
     
     public String get(String key, int timestamp) {
-        
+        if(!timeM.containsKey(key) || timeM.get(key).get(0) > timestamp)
+            return "";
+        List<Integer> times = timeM.get(key);
+        int a = 0, b = time.size();
+        while(a < b){
+            int n = (a + b) >> 1;
+            if(time.get(n) <= timestamp)
+                a = n + 1;
+            else 
+                b = n;
+        }
+        return valueM.get(key).get(a - 1);
     }
 }
