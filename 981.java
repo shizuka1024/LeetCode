c854233119bf5058e2840998e601b4e1dc45b1b1
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2023-06-15 17:16:36 +0800

    981.java

---
+++ 981.java
@@ -0,0 +1,18 @@
+class TimeMap {
+
+    Map<String , List<String>> valueM;
+    Map<String , List<Integer>> timeM;
+
+    public TimeMap() {
+        valueM = HashMap<>();
+        timeM = HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        
+    }
+    
+    public String get(String key, int timestamp) {
+        
+    }
+}
